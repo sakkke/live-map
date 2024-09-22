278a2212cb51570ab24c47e4908bf630cb3b7745
Konosuke Sakai <84666033+sakkke@users.noreply.github.com>
2024-09-22 15:33:09 +0900

    feat(page.tsx): implement geolocation to set initial map coordinates dynamically based on user's location

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,26 @@
+"use client";
+
 import { MapPageComponent } from "@/components/map-page";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [lng, setLng] = useState<number | null>(null);
+  const [lat, setLat] = useState<number | null>(null);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      setLng(position.coords.longitude);
+      setLat(position.coords.latitude);
+    })
+  }, []);
+
   return (
-    <MapPageComponent
-      initialLng={-70.9}
-      initialLat={42.35}
-      initialZoom={9}
-    />
+    <>
+      {lng && lat && <MapPageComponent
+        initialLng={lng}
+        initialLat={lat}
+        initialZoom={9}
+      />}
+    </>
   );
 }
